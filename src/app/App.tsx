12c32502517b30533885691474cd6c4e3801f96c
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-08-28 14:24:34 +0300

    feat:initialize project files and folders structure

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import {Button,Typography} from '@mui/material'
+function App() {
+
+  return (
+    <>
+    <Typography component='h1' variant='h4'>hello</Typography>
+    <Button variant="contained" color='error'>click</Button>
+    </>
+  )
+}
+
+export default App
