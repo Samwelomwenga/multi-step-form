b189f17bd68a7156fab7737266670b9a551e587e
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-09-05 09:41:27 +0300

    refact: change the Header jsx
    Refactor the jsx  for easy code reusability

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,51 +1,83 @@
-import {Box,Stepper,Step,StepLabel,useMediaQuery,Typography} from '@mui/material'
-import sidebarDesktop from '../../assets/images/bg-sidebar-desktop.svg'
-import sidebarMobile from '../../assets/images/bg-sidebar-mobile.svg'
-const Header:React.FC = () => {
-    const  isMediumScreen=useMediaQuery('(min-width:960px)');
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+
+import sidebarDesktop from "../../assets/images/bg-sidebar-desktop.svg";
+import sidebarMobile from "../../assets/images/bg-sidebar-mobile.svg";
+
+type typeStep = {
+  name: "STEP 1" | "STEP 2" | "STEP 3" | "STEP 4";
+  label: "YOUR INFO" | "SELECT PLAN" | "ADD-ONS" | "SUMMARY";
+};
+
+const Header: React.FC = () => {
+  const isMediumScreen = useMediaQuery("(min-width:960px)");
+
+  const steps: typeStep[] = [
+    {
+      name: "STEP 1",
+      label: "YOUR INFO",
+    },
+    {
+      name: "STEP 2",
+      label: "SELECT PLAN",
+    },
+    {
+      name: "STEP 3",
+      label: "ADD-ONS",
+    },
+    {
+      name: "STEP 4",
+      label: "SUMMARY",
+    },
+  ];
+
   return (
-    <Box component="header" sx={{ backgroundRepeat:'no-repeat',justifyContent:'center',height:{xs:'7.5rem',md:'100vh'},width:'100%', backgroundImage:{
-        xs:`url(${sidebarMobile})`,
-        md:`url(${sidebarDesktop})`
-    } }}>
-<Stepper  activeStep={0} nonLinear connector={null} orientation={isMediumScreen?'vertical':'horizontal'} 
->
-    <Step>
-        <StepLabel>
-        {isMediumScreen?<Box component='div'><Typography component='p' variant='body2'>STEP 1</Typography>
-            <Typography component='p' variant='body2'>YOUR INFO</Typography>
-        </Box>:''}
-            
-        </StepLabel>
-    </Step>
-    <Step>
-    <StepLabel>
-        {isMediumScreen?<Box component='div'><Typography component='p' variant='body2'>STEP 2</Typography>
-            <Typography component='p' variant='body2'>SELECT PLAN</Typography>
-        </Box>:''}
-            
-        </StepLabel>
-    </Step>
-    <Step>
-    <StepLabel>
-        {isMediumScreen?<Box component='div'><Typography component='p' variant='body2'>STEP 3</Typography>
-            <Typography component='p' variant='body2'>ADD-ONS</Typography>
-        </Box>:''}
-            
-        </StepLabel>
-    </Step>
-    <Step>
-    <StepLabel>
-        {isMediumScreen?<Box component='div'><Typography component='p' variant='body2'>STEP 4</Typography>
-            <Typography component='p' variant='body2'>SUMMARY</Typography>
-        </Box>:''}
-            
-        </StepLabel>
-    </Step>
-</Stepper>
-
+    <Box
+      component="header"
+      sx={{
+        backgroundRepeat: "no-repeat",
+        justifyContent: "center",
+        height: { xs: "7.5rem", md: "100vh" },
+        width: "100%",
+        backgroundImage: {
+          xs: `url(${sidebarMobile})`,
+          md: `url(${sidebarDesktop})`,
+        },
+      }}
+    >
+      <Stepper
+        activeStep={0}
+        nonLinear
+        connector={null}
+        orientation={isMediumScreen ? "vertical" : "horizontal"}
+      >
+        {steps.map((step) => (
+          <Step key={step.name}>
+            <StepLabel>
+              {isMediumScreen ? (
+                <Box component="div">
+                  <Typography component="p" variant="body2">
+                    {step.name}
+                  </Typography>
+                  <Typography component="p" variant="body2">
+                    {step.label}
+                  </Typography>
+                </Box>
+              ) : (
+                ""
+              )}
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
     </Box>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
