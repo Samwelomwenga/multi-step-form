ef487cfd83879c525970a9820c047b8f5434a657
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-09-06 11:09:00 +0300

    feat: add custom colors to ThankYouStep component
    Add the custom colors  to style the component according to the design

--- src/steps/ThankYouStep.tsx
+++ src/steps/ThankYouStep.tsx
@@ -1,11 +1,15 @@
-import {Stack} from '@mui/material'
+import {Stack,useTheme} from '@mui/material'
 import ThankYou from '../componets/ThankYou'
 import Header from '../features/header/Header'
 
 
 const ThankYouStep:React.FC = () => {
+  const theme = useTheme();
+  const neutral = theme.palette.neutral;
   return (
-    <Stack direction={{md:'row'}}  sx={{bgcolor:"grey", height:"100vh"}}>
+    <Stack direction={{md:'row'}}  sx={{bgcolor:{
+      xs:neutral.LightGray.main
+    }, height:"100vh",}}>
         <Header/>
         <ThankYou/>
     </Stack>
