2bcce69b8a916a6ef6997eb32f8bb72cee1bb83c
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-09-10 12:35:38 +0300

    feat: add selectPlan switch state to selectPlan component
    Add the selectPlan switch state to keep track of switch state on
    UI reaction

--- src/features/selectPlan/SelectPlan.tsx
+++ src/features/selectPlan/SelectPlan.tsx
@@ -9,11 +9,17 @@ import {
   useMediaQuery,
 } from "@mui/material";
 
+import { useForm, Controller } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+import { useSelector ,useDispatch} from "react-redux";
+import { selectPlanState, updateBilling } from "./selectPlanSlice";
+
 import ArcadeIcon from "../../assets/images/icon-arcade.svg";
 import AdvanceIcon from "../../assets/images/icon-advanced.svg";
 import ProIcon from "../../assets/images/icon-pro.svg";
 
-import Buttons from "../../componets/Buttons";
+import Buttons from "../../components/Buttons";
 
 interface plansInterface {
   name: "Arcade" | "Advance" | "Pro";
@@ -23,159 +29,190 @@ const SelectPlan: React.FC = () => {
   const theme = useTheme();
   const primary = theme.palette.Primary;
   const neutral = theme.palette.neutral;
-
   const isMediumScreen = useMediaQuery("(min-width:960px)");
+
+  const selectPlan = useSelector(
+    (state: { selectPlan: selectPlanState }) => state.selectPlan
+  );
+  const dispatch=useDispatch()
+  const handleSwitch = (value:boolean) => {
+    const updatedSelectPlanState: selectPlanState = {
+      ...selectPlan,
+      switchValue: value,
+    };
+dispatch(updateBilling(updatedSelectPlanState))
+  }
+  const {billing}=selectPlan
+  console.log(billing)
+
+  const { control } = useForm<selectPlanState>({ defaultValues: selectPlan });
 
   const plans: plansInterface[] = [
     {
       name: "Arcade",
-      icon: <ArcadeIcon/>,
+      icon: <ArcadeIcon />,
     },
     {
       name: "Advance",
-      icon: <AdvanceIcon/>,
+      icon: <AdvanceIcon />,
     },
     {
       name: "Pro",
-      icon: <ProIcon/>,
+      icon: <ProIcon />,
     },
   ];
   return (
-    <form noValidate autoComplete="off">
-      <RadioGroup
-        sx={{
-          gap: { xs: "1rem", md: "2rem" },
-          mb: "1.3rem",
-          display: { md: "inline-flex", flexDirection: "row" },
-        }}
-      >
-        {plans.map((plan) => (
-          <FormControlLabel
-            key={plan.name}
-            labelPlacement={isMediumScreen ? "bottom" : "end"}
-            sx={{
-              border: "2px solid",
-              borderColor: neutral.LightGray.main,
-              p: ".5rem",
-              pr: { md: "5rem" },
-              pl: {
-                md: "1rem",
-              },
-              py: {
-                md: "1.5rem",
-              },
-              ml: ".1rem",
-              borderRadius: ".5rem",
-              width: { xs: "100%", md: "auto" },
-            }}
-            label={
-              <Stack>
-                <Typography
-                  component="p"
-                  variant="body1"
-                  sx={{
-                    color: primary.MarineBlue.main,
-                    fontWeight: "700",
-                    mt: {
-                      md: "4rem",
-                    },
-                  }}
-                >
-                  {plan.name}
-                </Typography>
-                <Typography
-                  component="p"
-                  variant="body1"
-                  sx={{ color: neutral.CoolGray.main, fontWeight: "400" }}
-                >
-                  $90/Yr
-                </Typography>
-                <Typography
-                  component="p"
-                  variant="body2"
-                  sx={{ color: primary.MarineBlue.main, fontWeight: "700" }}
-                >
-                  2 months free
-                </Typography>
-              </Stack>
-            }
-            control={<Radio icon={plan.icon} />}
-          />
-        ))}
-      </RadioGroup>
-      <Stack
-        component="div"
-        direction="row"
-        sx={{
-          bgcolor: neutral.LightGray.main,
-          px: "3rem",
-          width: "100%",
-          borderRadius: ".5rem",
-          py: ".5rem",
-          alignItems: "center",
-          textAlign: "center",
-          mt: {
-            md: "2rem",
-          },
-          mb: "2rem",
-        }}
-      >
-        <Typography
-          component="p"
+    <>
+      <form noValidate autoComplete="off">
+        <RadioGroup
           sx={{
-            pl: { md: "13rem", fontWeight: "700" },
+            gap: { xs: "1rem", md: "2rem" },
+            mb: "1.3rem",
+            display: { md: "inline-flex", flexDirection: "row" },
           }}
         >
-          Monthly
-        </Typography>
-        <Switch
-          sx={{
-            "&.MuiSwitch-root": {
-              width: "4.3rem",
-            },
-            "&:active": {
-              "& .MuiSwitch-thumb": {
-                width: "1rem",
-              },
-              "& .MuiSwitch-switchBase.Mui-checked": {
-                transform: "translateX(1rem)",
-              },
-            },
-            "& .MuiSwitch-switchBase": {
-              padding: 2,
-              "&.Mui-checked": {
-                transform: "translateX(1.5rem)",
-                color: neutral.Alabaster.main,
-                "& + .MuiSwitch-track": {
-                  opacity: 1,
-                  backgroundColor: primary.MarineBlue.main,
+          {plans.map((plan) => (
+            <FormControlLabel
+              key={plan.name}
+              labelPlacement={isMediumScreen ? "bottom" : "end"}
+              sx={{
+                border: "2px solid",
+                borderColor: neutral.LightGray.main,
+                p: ".5rem",
+                pr: { md: "5rem" },
+                pl: {
+                  md: "1rem",
                 },
-              },
+                py: {
+                  md: "1.5rem",
+                },
+                ml: ".1rem",
+                borderRadius: ".5rem",
+                width: { xs: "100%", md: "auto" },
+              }}
+              label={
+                <Stack>
+                  <Typography
+                    component="p"
+                    variant="body1"
+                    sx={{
+                      color: primary.MarineBlue.main,
+                      fontWeight: "700",
+                      mt: {
+                        md: "4rem",
+                      },
+                    }}
+                  >
+                    {plan.name}
+                  </Typography>
+                  <Typography
+                    component="p"
+                    variant="body1"
+                    sx={{ color: neutral.CoolGray.main, fontWeight: "400" }}
+                  >
+                    $90/Yr
+                  </Typography>
+                  <Typography
+                    component="p"
+                    variant="body2"
+                    sx={{ color: primary.MarineBlue.main, fontWeight: "700" }}
+                  >
+                    2 months free
+                  </Typography>
+                </Stack>
+              }
+              control={<Radio icon={plan.icon} />}
+            />
+          ))}
+        </RadioGroup>
+        <Stack
+          component="div"
+          direction="row"
+          sx={{
+            bgcolor: neutral.LightGray.main,
+            px: "3rem",
+            width: "100%",
+            borderRadius: ".5rem",
+            py: ".5rem",
+            alignItems: "center",
+            textAlign: "center",
+            mt: {
+              md: "2rem",
             },
-            "& .MuiSwitch-thumb": {
-              width: ".8rem",
-              height: ".8rem",
-              borderRadius: "50%",
-              transition: theme.transitions.create(["width"], {
-                duration: 200,
-              }),
-            },
-            "& .MuiSwitch-track": {
-              opacity: 1,
-              height: "1.5rem",
-              borderRadius: "10rem",
-              width: "7.5rem",
-              backgroundColor: primary.MarineBlue.main,
-              boxSizing: "border-box",
-            },
+            mb: "2rem",
           }}
-        />
-        <Typography component="p" sx={{ fontWeight: "700" }}>
-          Yearly
-        </Typography>
-      </Stack>
-      <Buttons />
-    </form>
+        >
+          <Typography
+            component="p"
+            sx={{
+              pl: { md: "13rem", fontWeight: "700",color:billing==="monthly"?primary.MarineBlue.main:neutral.CoolGray.main },
+            }}
+          >
+            Monthly
+          </Typography>
+          <Controller
+            name="switchValue" // Type '"billingSwitch"' is not assignable to type '"switch" | "plan" | "billing" | "switchValue"'.ts(2322)
+            control={control}
+            // defaultValue={false}
+            render={({ field }) => (
+              <Switch
+                {...field}
+                onChange={(event)=>{
+                  field.onChange(event.target.checked)
+                  handleSwitch(event.target.checked)
+                }}
+                sx={{
+                  "&.MuiSwitch-root": {
+                    width: "4.3rem",
+                  },
+                  "&:active": {
+                    "& .MuiSwitch-thumb": {
+                      width: "1rem",
+                    },
+                    "& .MuiSwitch-switchBase.Mui-checked": {
+                      transform: "translateX(1rem)",
+                    },
+                  },
+                  "& .MuiSwitch-switchBase": {
+                    padding: 2,
+                    "&.Mui-checked": {
+                      transform: "translateX(1.5rem)",
+                      color: neutral.Alabaster.main,
+                      "& + .MuiSwitch-track": {
+                        opacity: 1,
+                        backgroundColor: primary.MarineBlue.main,
+                      },
+                    },
+                  },
+                  "& .MuiSwitch-thumb": {
+                    width: ".8rem",
+                    height: ".8rem",
+                    borderRadius: "50%",
+                    transition: theme.transitions.create(["width"], {
+                      duration: 200,
+                    }),
+                  },
+                  "& .MuiSwitch-track": {
+                    opacity: 1,
+                    height: "1.5rem",
+                    borderRadius: "10rem",
+                    width: "7.5rem",
+                    backgroundColor: primary.MarineBlue.main,
+                    boxSizing: "border-box",
+                  },
+                }}
+              />
+            )}
+          />
+
+          <Typography component="p" sx={{ fontWeight: "700",color:billing==="yearly"?primary.MarineBlue.main:neutral.CoolGray.main}}>
+            Yearly
+          </Typography>
+        </Stack>
+        <Buttons />
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 
--- src/features/selectPlan/selectPlanSlice.ts
+++ src/features/selectPlan/selectPlanSlice.ts
@@ -1,25 +1,29 @@
-import { createSlice,PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export type selectPlanState={
-    plan:"arcade"|"advance"|"pro",
-    billing:"monthly"|"yearly"
-}
+export type selectPlanState = {
+  plan: "arcade" | "advance" | "pro";
+  billing: "monthly" | "yearly";
+  switchValue: boolean;
+};
 
 const selectPlanSlice = createSlice({
   name: "selectPlanSlice",
   initialState: {
     plan: "arcade",
     billing: "monthly",
-  }  as selectPlanState,
-  reducers:{
-    updatePlan:(state,action:PayloadAction<selectPlanState>)=>{
-        return{...state, ...action.payload}
+    switchValue:false,
+  } as selectPlanState,
+  reducers: {
+    updatePlan: (state, action: PayloadAction<selectPlanState>) => {
+      return { ...state, ...action.payload };
     },
-    updateBilling:(state,action:PayloadAction<selectPlanState>)=>{
-        return{...state, ...action.payload}
-    }
-
-  }
+    updateBilling: (state, action: PayloadAction<selectPlanState>) => {
+      const { switchValue } = action.payload;
+      const billing = switchValue ? "yearly" : "monthly";
+      // state.billing=billing
+      return { ...state, billing };
+    },
+  },
 });
-export const {updatePlan,updateBilling}=selectPlanSlice.actions;
+export const { updatePlan, updateBilling } = selectPlanSlice.actions;
 export default selectPlanSlice.reducer;
