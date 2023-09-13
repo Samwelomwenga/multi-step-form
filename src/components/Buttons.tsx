95e7e5cc5322ea230c5167f6deed9318502af3f4
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-09-13 21:15:18 +0300

    fix:change component folder name
    Change the folder name  to remove a typo

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,84 @@
+import { Button, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+// import { useNavigate } from 'react-router-dom'
+
+const NextButton: React.FC = () => {
+  const theme = useTheme();
+  const primary = theme.palette.Primary;
+  const neutral = theme.palette.neutral;
+
+  return (
+    <Button
+      type="submit"
+      variant="contained"
+      size="small"
+      sx={{
+        bgcolor: primary.MarineBlue.main,
+        color: neutral.Magnolia.main,
+        py: ".4rem",
+        px: ".8rem",
+        ":hover": { bgcolor: primary.MarineBlue.light },
+      }}
+    >
+      Next Step
+    </Button>
+  );
+};
+const BackButton: React.FC = () => {
+  const theme = useTheme();
+  const primary = theme.palette.Primary;
+  const neutral = theme.palette.neutral;
+  // const navigate=useNavigate()
+  // const handleBackButton = () => {
+  //     navigate(-1)
+  // }
+  return (
+    <Button
+      variant="text"
+      size="small"
+      sx={{
+        color: neutral.CoolGray.main,
+        fontWeight: "700",
+        py: ".4rem",
+        px: ".8rem",
+        ":hover": {
+          color: primary.MarineBlue.main,
+        },
+      }}
+    >
+      Go Back
+    </Button>
+  );
+};
+
+const Buttons: React.FC = () => {
+  const theme = useTheme();
+  // const primary = theme.palette.Primary;
+  const neutral = theme.palette.neutral;
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        width:{xs:"100%",md:"55%"},
+        flexDirection: "row",
+        justifyContent: "space-between",
+        position:"absolute",
+        bottom:{xs:"0",
+      md:"10%"},
+        bgcolor:neutral.White.main,
+        py:"1rem",
+        px:{
+          xs:".7rem",
+          md:"0"
+        },
+        ml:{xs:"-2rem",
+      md:"0"}
+      }}
+    >
+      <BackButton />
+      <NextButton />
+    </Stack>
+  );
+};
+
+export default Buttons;
---
+++ src/components/ThankYou.tsx
@@ -0,0 +1,26 @@
+import {Card,CardContent,CardMedia,Typography,Stack,useTheme} from '@mui/material'
+import iconThankYou from '../assets/images/icon-thank-you.svg'
+const ThankYou:React.FC = () => {
+  const theme = useTheme();
+  const primary = theme.palette.Primary;
+  const neutral = theme.palette.neutral;
+  return (
+    <Card sx={{placeItems:"center",textAlign:"center",mx:"1rem",py:"2rem",px:"1rem",mt:{xs:"-3rem",md:"0",backgroundColor:neutral.White.main}}}>
+        <Stack sx={{mt:"2rem"}}>
+        <CardMedia component='img' sx={{maxWidth:"4rem",mx:"auto"}} image={iconThankYou} alt='Thank You Icon'/>
+        <CardContent>
+            <Typography component='h3' variant='h5' sx={{my:"1rem",color:primary.MarineBlue.main,fontWeight:"700"}}> Thank you! </Typography>
+            <Typography paragraph sx={{color:neutral.CoolGray.main}}>
+            Thanks for confirming your subscription! 
+            We hope you have fun using our 
+            platform. If you ever need support,
+             please feel free to email us at
+              support@loremgaming.com. 
+            </Typography>
+        </CardContent>
+        </Stack>
+    </Card>
+  )
+}
+
+export default ThankYou
